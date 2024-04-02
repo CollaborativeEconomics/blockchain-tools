b91edffed858595a3ad69bb7ba53faa2278e3d76
Julian Kingman <julian.kingman@gmail.com>
2024-04-01 22:31:26 -0500

    disable xummclient export

--- src/Wallets/index.ts
+++ src/Wallets/index.ts
@@ -1,6 +1,10 @@
 import FreighterWallet from './freighter';
 import MetaMaskWallet from './metamask';
-import XummClient from './xummclient';
+// import XummClient from './xummclient';
 import XummServer from './xummserver';
 
-export { FreighterWallet, MetaMaskWallet, XummClient, XummServer };
+export {
+  FreighterWallet, MetaMaskWallet,
+  // XummClient,
+  XummServer
+};
